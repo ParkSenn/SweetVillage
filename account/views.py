400d865de66d2a3acc5339876a58b0b6b67c9947
Hansuyeon2 <hansuyeon23@naver.com>
2023-02-18 00:17:17 +0900

    login 추가

--- account/views.py
+++ account/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from account.forms import UserForm
 
-# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        usr = request.POST['username']
+        pwd = request.POST['password']
+        user = auth.authenticate(request, username=usr, password=pwd)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('sweetvillage')
+        else:
+            return render(request, 'login.html')
+    elif request.method == 'GET':
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('sweetvillage')
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=raw_password)
+            auth.login(request,user)
+            return redirect('sweetvillage')
+
+    else:
+        form = UserForm()
+    return render(request, 'signup.html',{'form':form}
+    )       
--- sweetvillage/urls.py
+++ sweetvillage/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from sweetapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/',include('account.urls')),
     # runserver 테스트 용 path('', views.testapp, name='home'),
 ]
